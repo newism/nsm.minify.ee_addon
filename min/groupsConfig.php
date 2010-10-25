7a8812f85a9e4f7848be9cd6c8b4e82df201ddbc
Leevi Graham <leevi@newism.com.au>
2010-10-26 09:44:30 +1100

    Added css_print group to page

--- min/groupsConfig.php
+++ min/groupsConfig.php
@@ -10,22 +10,26 @@
  **/
 
 return array(
-		'css_screen' => array(
-			'//templates/default_site/css/screen/base.css',
-			'//templates/default_site/css/screen/layout.css',
-			'//templates/default_site/css/screen/typography.css',
-			'//templates/default_site/css/screen/forms.css',
-			'//templates/default_site/css/screen/tables.css',
-			'//templates/default_site/css/screen/navigation.css',
-			'//templates/default_site/css/screen/entries.css',
-			'//templates/default_site/css/screen/comments.css',
-			'//templates/default_site/css/screen/widgets.css',
-			'//templates/default_site/css/screen/adverts.css',
-			
-		),
+	'css_screen' => array(
+		'//templates/default_site/css/screen/base.css',
+		'//templates/default_site/css/screen/layout.css',
+		'//templates/default_site/css/screen/typography.css',
+		'//templates/default_site/css/screen/forms.css',
+		'//templates/default_site/css/screen/tables.css',
+		'//templates/default_site/css/screen/navigation.css',
+		'//templates/default_site/css/screen/entries.css',
+		'//templates/default_site/css/screen/comments.css',
+		'//templates/default_site/css/screen/widgets.css',
+		'//templates/default_site/css/screen/adverts.css',
+	
+	),
+	'css_print' => array(
+		'//templates/default_site/css/screen/base.css',
+		'//templates/default_site/css/screen/typography.css',
+	
+	),
 		'js' => array(
 			'//templates/default_site/js/jquery.min.js',
-			'//templates/default_site/js/jquery-nsm_TOC.js',
 			'//templates/default_site/js/behaviours.js'
 		)
 );
